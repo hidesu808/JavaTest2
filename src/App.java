0ad5202de47a1f0560e5f681367e0a186351d000
Sedy Agustin <agustin.sedy@gmail.com>
2023-04-23 20:01:35 -0400

    Done commiting

--- src/App.java
+++ src/App.java
@@ -3,20 +3,30 @@ import java.io.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        
+        //TESTING DEMO
         System.out.println("Hello, World!");
         ArrayList<Coordinates> list = new ArrayList<Coordinates>();
+        list.add(new Coordinates("A", -84.032126, 34.071238));
+        list.add(new Coordinates("B", -84.032041, 34.070831));
+        list.add(new Coordinates("C", -84.031311, 34.071022));
+        list.add(new Coordinates("D", -84.031433, 34.070442));
+
+        Coordinates destination = new Coordinates("ME", -84.031479, 34.070914);
+
+        minDistance(list, destination);
     }
 
     public static void printCoordinates(Coordinates coordinates) {
         System.out.println("Name: " + coordinates.getName());
-        System.out.println("Lang: " + coordinates.getLang());
+        System.out.println("Lang: " + coordinates.getLon());
         System.out.println("Lat: " + coordinates.getLat());
     }
     public static double distance(Coordinates origin , Coordinates destination) {
         double lat1 = origin.getLat();
         double lat2 = destination.getLat();
-        double lon1 = origin.getLang();
-        double lon2 = destination.getLang();
+        double lon1 = origin.getLon();
+        double lon2 = destination.getLon();
         //We are ignoring height difference when calculating distance for distance using lat and long
         double height = 0;
         final int R = 6371; // Radius of the earth
@@ -47,7 +57,8 @@ public class App {
         for(int i = 0; i < origin.size(); i++) {
             double temp = distance(origin.get(i), destination);
             if (temp == min) {
-                System.out.println("The closest point is: ");
+                System.out.println("The closest point with the distance of "
+                 + min +" meter is: ");
                 printCoordinates(origin.get(i));
             }
         }
